875455510facb7481832036e6b9f3dacd11aa5e9
Cong Dinh <congdinhvannguyen165@outlook.com>
2022-11-12 12:13:32 +0700

    Stack array completed

---
+++ stackArray/MyStack.java
@@ -0,0 +1,70 @@
+/**
+ * MyStack
+ */
+public class MyStack {
+
+    private int capacity;
+    private int top;
+    private int items[];
+
+    MyStack(int capacity) {
+        this.capacity = capacity;
+        this.items = new int[capacity];
+        this.top = -1;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    // Check if the stack is empty
+    boolean isEmpty() {
+        return top < 0;
+    }
+
+    // Check if the stack is full
+    boolean isFull() {
+        return top >= capacity - 1;
+    }
+
+    // Push value to stack
+    public boolean push(int x) {
+        if (isFull()) {
+            System.out.println("Stack Overflow: Cannot push " + x);
+            return false;
+        } else {
+            items[++top] = x;
+            return true;
+        }
+    }
+
+    // Pop value from stack
+    public int pop() {
+        if (isEmpty()) {
+            System.out.println("Stack is empty");
+            return 0;
+        } else {
+            int x = items[top--];
+            return x;
+        }
+    }
+
+    // Peek value from stack
+    public int peek() {
+        if (isEmpty()) {
+            System.out.println("Stack is empty");
+            return 0;
+        } else {
+            int x = items[top];
+            return x;
+        }
+    }
+
+    // List value from stack
+    public void traverse() {
+        System.out.println("Stack elements:");
+        for (int i = top; i > -1; i--) {
+            System.out.println(items[i]);
+        }
+    }
+}
---
+++ stackArray/StackTest.java
@@ -0,0 +1,25 @@
+public class StackTest {
+    public static void main(String[] args) {
+        // Create stack with capacity
+        MyStack stack = new MyStack(5);
+
+        // Push value to stack
+        stack.push(3);
+        stack.push(5);
+        stack.push(7);
+        stack.push(9);
+        stack.push(11);
+
+        System.out.println("Stack capacity: " + stack.getCapacity());
+
+        // List values from stack
+        stack.traverse();
+
+        System.out.println("Test pop:");
+        System.out.println(stack.pop());
+
+        System.out.println("Test peek:");
+        System.out.println(stack.peek());
+        System.out.println(stack.peek());
+    }
+}
